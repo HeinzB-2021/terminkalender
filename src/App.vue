29a6cf0de9212f7791d8e72c375e067ed6774ae9
Heinz Bartenstein <info@heinzbartenstein.de>
2022-05-20 17:23:34 +0200

    Weitere Componenten

--- src/App.vue
+++ src/App.vue
@@ -11,45 +11,7 @@
     <div class="row mt-3">
       <div class="col-4 offset-4">
         <!-- Anfang: Template für die Calendar-Entry-Component -->
-        <div id="calender-entry">
-          <div class="card">
-            <div class="card-header text-center">
-              <h5>
-                Neuer Termin für: <strong>Montag</strong>
-              </h5>
-            </div>
-            <div class="card-body">
-              <input
-                type="text"
-                class="form-control"
-                placeholder="Neuer Eintrag"
-              />
-              <select class="form-select mt-2">
-                <option value="-1">Hoch</option>
-                <option value="0">Mittel</option>
-                <option value="1">Tief</option>
-              </select>
-              <div class="text-center mt-3">
-                <span
-                  v-for="i in 5"
-                  :key="i"
-                  class="d-inline-block alert alert-primary m-0 me-2 square"
-                  role="button"
-                >
-                </span>
-              </div>
-              <hr />
-              <div class="d-grid gap-2">
-                <button class="btn btn-primary">
-                  Eintragen
-                </button>
-                <button class="btn btn-danger">
-                  Inhalt löschen
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <calendar-entry></calendar-entry>
         <!-- Ende: Template für die Calendar-Day-Component -->
       </div>
       <div class="col-2 offset-2">
@@ -76,93 +38,7 @@
           </button>
         </div>
         <!-- Anfang: Template für die Calendar-Settings-Component -->
-        <div id="calendar-settings">
-          <div class="card">
-            <div
-              class="card-header text-center bg-success text-white"
-            >
-              <strong>Einstellungen</strong>
-            </div>
-            <div class="card-body">
-              <ul class="nav nav-pills nav-fill">
-                <li
-                  v-for="i in 2"
-                  :key="i"
-                  class="nav-item"
-                  role="button"
-                >
-                  <a class="nav-link"
-                    ><i class="fas fa-icons text-success">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        class="bi bi-journal-text"
-                        viewBox="0 0 16 16"
-                      >
-                        <path
-                          d="M5 10.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"
-                        />
-                        <path
-                          d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"
-                        />
-                        <path
-                          d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"
-                        /></svg></i
-                  ></a>
-                </li>
-              </ul>
-              <hr />
-              <ul class="nav nav-pills nav-fill">
-                <li class="nav-item" role="button">
-                  <a class="nav-link"
-                    ><i
-                      class="fas fa-sort-numeric-down-alt text-success"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        class="bi bi-sort-numeric-down-alt"
-                        viewBox="0 0 16 16"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M11.36 7.098c-1.137 0-1.708-.657-1.762-1.278h1.004c.058.223.343.45.773.45.824 0 1.164-.829 1.133-1.856h-.059c-.148.39-.57.742-1.261.742-.91 0-1.72-.613-1.72-1.758 0-1.148.848-1.836 1.973-1.836 1.09 0 2.063.637 2.063 2.688 0 1.867-.723 2.848-2.145 2.848zm.062-2.735c.504 0 .933-.336.933-.972 0-.633-.398-1.008-.94-1.008-.52 0-.927.375-.927 1 0 .64.418.98.934.98z"
-                        />
-                        <path
-                          d="M12.438 8.668V14H11.39V9.684h-.051l-1.211.859v-.969l1.262-.906h1.046zM4.5 2.5a.5.5 0 0 0-1 0v9.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L4.5 12.293V2.5z"
-                        /></svg></i
-                  ></a>
-                </li>
-                <li class="nav-item" role="button">
-                  <a class="nav-link"
-                    ><i
-                      class="fas fa-sort-alpha-down text-success"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        class="bi bi-sort-alpha-down"
-                        viewBox="0 0 16 16"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M10.082 5.629 9.664 7H8.598l1.789-5.332h1.234L13.402 7h-1.12l-.419-1.371h-1.781zm1.57-.785L11 2.687h-.047l-.652 2.157h1.351z"
-                        />
-                        <path
-                          d="M12.96 14H9.028v-.691l2.579-3.72v-.054H9.098v-.867h3.785v.691l-2.567 3.72v.054h2.645V14zM4.5 2.5a.5.5 0 0 0-1 0v9.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L4.5 12.293V2.5z"
-                        /></svg></i
-                  ></a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
+        <calendar-settings></calendar-settings>
         <!-- Ende: Template für die Calendar-Day-Component -->
       </div>
     </div>
@@ -171,10 +47,14 @@
 
 <script>
 import CalendarWeek from "@/components/CalendarWeek";
+import CalendarEntry from "@/components/CalendarEntry";
+import CalendarSettings from "@/components/CalendarSettings";
 export default {
   name: "App",
   components: {
     CalendarWeek,
+    CalendarEntry,
+    CalendarSettings,
   },
 };
 </script>
